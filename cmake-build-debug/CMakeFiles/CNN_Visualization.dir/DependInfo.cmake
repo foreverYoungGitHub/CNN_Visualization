9a70799875d8d748de2dc521b1630fd6cf155b4c
刘阳 <Young@liuyangdeMacBook-Pro.local>
2016-11-29 23:46:55 -0500

    initial imgui

---
+++ cmake-build-debug/CMakeFiles/CNN_Visualization.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Young/projects/CNN_Visualization/deps/imgui/examples/libs/gl3w/GL/gl3w.c" "/Users/Young/projects/CNN_Visualization/cmake-build-debug/CMakeFiles/CNN_Visualization.dir/deps/imgui/examples/libs/gl3w/GL/gl3w.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../deps/glfw/include"
+  "../deps/glfw/deps"
+  "../deps/imgui"
+  "../deps/imgui/examples/libs/gl3w"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Young/projects/CNN_Visualization/deps/imgui/examples/opengl3_example/imgui_impl_glfw_gl3.cpp" "/Users/Young/projects/CNN_Visualization/cmake-build-debug/CMakeFiles/CNN_Visualization.dir/deps/imgui/examples/opengl3_example/imgui_impl_glfw_gl3.cpp.o"
+  "/Users/Young/projects/CNN_Visualization/deps/imgui/examples/opengl3_example/main.cpp" "/Users/Young/projects/CNN_Visualization/cmake-build-debug/CMakeFiles/CNN_Visualization.dir/deps/imgui/examples/opengl3_example/main.cpp.o"
+  "/Users/Young/projects/CNN_Visualization/deps/imgui/imgui.cpp" "/Users/Young/projects/CNN_Visualization/cmake-build-debug/CMakeFiles/CNN_Visualization.dir/deps/imgui/imgui.cpp.o"
+  "/Users/Young/projects/CNN_Visualization/deps/imgui/imgui_demo.cpp" "/Users/Young/projects/CNN_Visualization/cmake-build-debug/CMakeFiles/CNN_Visualization.dir/deps/imgui/imgui_demo.cpp.o"
+  "/Users/Young/projects/CNN_Visualization/deps/imgui/imgui_draw.cpp" "/Users/Young/projects/CNN_Visualization/cmake-build-debug/CMakeFiles/CNN_Visualization.dir/deps/imgui/imgui_draw.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../deps/glfw/include"
+  "../deps/glfw/deps"
+  "../deps/imgui"
+  "../deps/imgui/examples/libs/gl3w"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/Young/projects/CNN_Visualization/cmake-build-debug/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CNN_Visualization.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CNN_Visualization.dir/deps/imgui/examples/libs/gl3w/GL/gl3w.c.o"
+  "CMakeFiles/CNN_Visualization.dir/deps/imgui/imgui.cpp.o"
+  "CMakeFiles/CNN_Visualization.dir/deps/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/CNN_Visualization.dir/deps/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/CNN_Visualization.dir/deps/imgui/examples/opengl3_example/imgui_impl_glfw_gl3.cpp.o"
+  "CMakeFiles/CNN_Visualization.dir/deps/imgui/examples/opengl3_example/main.cpp.o"
+  "CNN_Visualization.pdb"
+  "CNN_Visualization"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CNN_Visualization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
